85249958cec24b14659f3b2267c6d747bb874417
unknown <shashankk@GEBHSTC3.bitwiseglobal.net>
2015-08-11 19:07:48 +0530

    unittest

---
+++ com.bitwise.app.unitest/src/com/bitwise/app/unitest/SampleTest.java
@@ -0,0 +1,17 @@
+package com.bitwise.app.unitest;
+
+
+	import static org.junit.Assert.*;
+
+	import org.junit.Test;
+
+	public class SampleTest {
+
+	  @Test
+	  public void test() {
+	    // just an example
+	    assertTrue(true);
+	  }
+
+	} 
+
