2ba4e90cdaa63d7a7383a30a7df81a4a8ebd0ccc
rahulma <rahulma@GEBHSTC15.bitwiseglobal.net>
2015-08-31 17:36:00 +0530

    Updated to apply common policies

--- com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/components/part/ComponentEditPart.java
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/components/part/ComponentEditPart.java
@@ -1,148 +1,31 @@
 package com.bitwise.app.graph.components.part;
 
-import java.awt.Label;
 import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.List;
 
 import org.eclipse.draw2d.ChopboxAnchor;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.ConnectionAnchor;
-import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.ImageFigure;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.ConnectionEditPart;
-import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.NodeEditPart;
 import org.eclipse.gef.Request;
-import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
-import org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy;
-import org.eclipse.gef.requests.CreateConnectionRequest;
-import org.eclipse.gef.requests.ReconnectRequest;
 
 import com.bitwise.app.common.util.ComponentCacheUtil;
-import com.bitwise.app.graph.components.commands.ConnectionCreateCommand;
-import com.bitwise.app.graph.components.commands.ConnectionReconnectCommand;
 import com.bitwise.app.graph.components.dynamic.DynamicClassProcessor;
-import com.bitwise.app.graph.components.editpolicies.AppRenamePolicy;
-import com.bitwise.app.graph.components.editpolicies.GraphComponentEditPolicy;
 import com.bitwise.app.graph.components.figure.ComponentFigure;
 import com.bitwise.app.graph.components.model.Component;
-import com.bitwise.app.graph.components.model.Connection;
 import com.bitwise.app.graph.components.model.GenericComponent;
-import com.bitwise.app.graph.components.model.ModelElement;
 
 
-public class ComponentEditPart extends AbstractGraphicalEditPart implements
-PropertyChangeListener, NodeEditPart {
+public class ComponentEditPart extends CommonPart implements
+ NodeEditPart {
 	
 	private ConnectionAnchor anchor;
 
-	/**
-	 * Upon activation, attach to the model element as a property change
-	 * listener.
-	 */
-	public void activate() {
-		if (!isActive()) {
-			super.activate();
-			((ModelElement) getModel()).addPropertyChangeListener(this);
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.editparts.AbstractEditPart#createEditPolicies()
-	 */
-	protected void createEditPolicies() {
-		// allow removal of the associated model element
-		installEditPolicy(EditPolicy.COMPONENT_ROLE,
-				new GraphComponentEditPolicy());
-		
-		installEditPolicy(EditPolicy.NODE_ROLE, new AppRenamePolicy());
-		
-		
-		// allow the creation of connections and
-		// and the reconnection of connections between Shape instances
-		installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE,
-				new GraphicalNodeEditPolicy() {
-					/*
-					 * (non-Javadoc)
-					 * 
-					 * @see
-					 * org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#
-					 * getConnectionCompleteCommand
-					 * (org.eclipse.gef.requests.CreateConnectionRequest)
-					 */
-					protected Command getConnectionCompleteCommand(
-							CreateConnectionRequest request) {
-						ConnectionCreateCommand cmd = (ConnectionCreateCommand) request
-								.getStartCommand();
-						cmd.setTarget((Component) getHost().getModel());
-						return cmd;
-					}
-
-					/*
-					 * (non-Javadoc)
-					 * 
-					 * @see
-					 * org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#
-					 * getConnectionCreateCommand
-					 * (org.eclipse.gef.requests.CreateConnectionRequest)
-					 */
-					protected Command getConnectionCreateCommand(
-							CreateConnectionRequest request) {
-						Component source = (Component) getHost().getModel();
-						int style = ((Integer) request.getNewObjectType())
-								.intValue();
-						ConnectionCreateCommand cmd = new ConnectionCreateCommand(
-								source, style);
-						request.setStartCommand(cmd);
-						return cmd;
-					}
-
-					/*
-					 * (non-Javadoc)
-					 * 
-					 * @see
-					 * org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#
-					 * getReconnectSourceCommand
-					 * (org.eclipse.gef.requests.ReconnectRequest)
-					 */
-					protected Command getReconnectSourceCommand(
-							ReconnectRequest request) {
-						Connection conn = (Connection) request
-								.getConnectionEditPart().getModel();
-						Component newSource = (Component) getHost().getModel();
-						ConnectionReconnectCommand cmd = new ConnectionReconnectCommand(
-								conn);
-						cmd.setNewSource(newSource);
-						return cmd;
-					}
-
-					/*
-					 * (non-Javadoc)
-					 * 
-					 * @see
-					 * org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#
-					 * getReconnectTargetCommand
-					 * (org.eclipse.gef.requests.ReconnectRequest)
-					 */
-					protected Command getReconnectTargetCommand(
-							ReconnectRequest request) {
-						Connection conn = (Connection) request
-								.getConnectionEditPart().getModel();
-						Component newTarget = (Component) getHost().getModel();
-						ConnectionReconnectCommand cmd = new ConnectionReconnectCommand(
-								conn);
-						cmd.setNewTarget(newTarget);
-						return cmd;
-					}
-				});
-	}
+	
 
 	/*
 	 * (non-Javadoc)
@@ -181,17 +64,6 @@ PropertyChangeListener, NodeEditPart {
 		}*/else {
 			// if Shapes gets extended the conditions above must be updated
 			throw new IllegalArgumentException();
-		}
-	}
-
-	/**
-	 * Upon deactivation, detach from the model element as a property change
-	 * listener.
-	 */
-	public void deactivate() {
-		if (isActive()) {
-			super.deactivate();
-			((ModelElement) getModel()).removePropertyChangeListener(this);
 		}
 	}
 
