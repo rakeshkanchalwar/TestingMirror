2dfe29c07f26cf58450e6b5a491f4a707d19574f
unknown <rakeshka@GEBHSTC8.bitwiseglobal.net>
2015-09-18 15:07:42 +0530

    cleanup

--- com.bitwise.app.common/src/test/java/TestClass.java
+++
@@ -1,9 +0,0 @@
-package test.java;
-
-public class TestClass {
-	public static void main(String[] args) throws ClassNotFoundException {
-		TestClass class1 = new TestClass();
-		Class<?> clazz = class1.getClass().getClassLoader().loadClass("test.java.TestGeneralPolices");
-		System.out.println(clazz);
-	}
-}
