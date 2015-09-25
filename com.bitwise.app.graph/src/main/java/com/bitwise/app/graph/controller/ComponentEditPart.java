81b8475dd169874c36d1995d3ff301244a423f6b
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2015-09-25 17:06:56 +0530

    Fixed property window scroll issues. Added Apply functionality to property window. Added confirmation dialog on property window cancel button click. Integrated new property window with graph. made build specific changes.

--- com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/controller/ComponentEditPart.java
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/controller/ComponentEditPart.java
@@ -3,7 +3,6 @@ package com.bitwise.app.graph.controller;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.draw2d.ColorConstants;
@@ -20,24 +19,17 @@ import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 import org.eclipse.gef.editpolicies.AbstractEditPolicy;
 import org.eclipse.gef.requests.DropRequest;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.xml.sax.SAXException;
 
 import com.bitwise.app.common.component.config.Policy;
 import com.bitwise.app.common.util.XMLConfigUtil;
-import com.bitwise.app.eltproperties.adapters.ELTComponentPropertyAdapter;
-import com.bitwise.app.eltproperties.exceptions.EmptyComponentPropertiesException;
-import com.bitwise.app.eltproperties.property.IPropertyTreeBuilder;
-import com.bitwise.app.eltproperties.property.Property;
-import com.bitwise.app.eltproperties.property.PropertyTreeBuilder;
-import com.bitwise.app.eltproperties.propertydialog.PropertyDialog;
 import com.bitwise.app.graph.figure.ComponentFigure;
 import com.bitwise.app.graph.figure.factory.ModelFigureFactory;
 import com.bitwise.app.graph.model.Component;
 import com.bitwise.app.graph.model.Link;
 import com.bitwise.app.graph.processor.DynamicClassProcessor;
+import com.bitwise.app.graph.propertywindow.ELTPropertyWindow;
+//import com.bitwise.app.graph.propertywindow.ProdELTPropertyWindow;
 
 public class ComponentEditPart extends AbstractGraphicalEditPart implements
 		NodeEditPart, PropertyChangeListener {
@@ -223,36 +215,13 @@ public class ComponentEditPart extends AbstractGraphicalEditPart implements
 	public void performRequest(Request req) {
 		// Opens Property Window only on Double click.
 		if (req.getType().equals(RequestConstants.REQ_OPEN)) {
-			String componentName = DynamicClassProcessor.INSTANCE.getClazzName(getModel().getClass());
-			Object rowProperties = XMLConfigUtil.INSTANCE.getComponent(componentName).getProperty();
-
-			Component component = (Component) getModel();
-			// Property Window will blink if we try to click outside without closing.
-
-			ELTComponentPropertyAdapter eltComponentPropertyAdapter = new ELTComponentPropertyAdapter(rowProperties);
-			try {
-				eltComponentPropertyAdapter.transform();
-				Display display = Display.getDefault();
-				Shell shell = new Shell(display.getActiveShell(), SWT.WRAP | SWT.APPLICATION_MODAL);//
-
-				ArrayList<Property> componentProperties = eltComponentPropertyAdapter.getProperties();
-				IPropertyTreeBuilder propertyTreeBuilder = new PropertyTreeBuilder(componentProperties);
-				System.out.println(propertyTreeBuilder.toString());
-				PropertyDialog testwindow = new PropertyDialog(shell, propertyTreeBuilder.getPropertyTree(),
-						component.getProperties(), component.getParent().getComponentNames());
-				testwindow.open();
-				
-				int w = ((String) getCastedModel().getPropertyValue("name")).length()*7+40;
-				int defaultWidth = (getCastedModel().getBasename().length()+3)*7+40;
-				Dimension newSize = new Dimension(w < defaultWidth ? defaultWidth : w, 60);
-				getCastedModel().setSize(newSize);
-				refreshVisuals();
-				getFigure().repaint();
-
-			} catch (EmptyComponentPropertiesException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			ELTPropertyWindow eltPropertyWindow = new ELTPropertyWindow(getModel());
+			//ProdELTPropertyWindow eltPropertyWindow = new ProdELTPropertyWindow(getModel());
+			eltPropertyWindow.open();
+			
+			refreshVisuals();
+			getFigure().repaint();
+			
 			super.performRequest(req);
 		}
 	}
---
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/propertywindow/IELTPropertyWindow.java
@@ -0,0 +1,12 @@
+package com.bitwise.app.graph.propertywindow;
+
+/**
+ * 
+ * @author Shrirang S. Kumbhar
+ * Sep 25, 2015
+ * 
+ */
+
+public interface IELTPropertyWindow {
+	public void open();
+}
