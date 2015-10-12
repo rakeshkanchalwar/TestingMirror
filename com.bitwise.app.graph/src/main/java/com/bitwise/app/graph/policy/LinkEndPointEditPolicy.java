83f82b1af08b75c1c47d3ddf91fd8c999ea339cc
kanchank <kanchank@GEBHSTC1.bitwiseglobal.net>
2015-10-12 19:39:10 +0530

    1. LinkEndPointPolicy class for changing color of link on select. 2. Add link color change policy in link edit part. 3. Code refactoring in component border.

---
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/policy/LinkEndPointEditPolicy.java
@@ -0,0 +1,21 @@
+package com.bitwise.app.graph.policy;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.gef.GraphicalEditPart;
+import org.eclipse.gef.editpolicies.ConnectionEndpointEditPolicy;
+
+public class LinkEndPointEditPolicy extends ConnectionEndpointEditPolicy{
+
+	protected void addSelectionHandles() {
+		getLinkFigure().setForegroundColor(ColorConstants.blue);
+	}
+
+	protected PolylineConnection getLinkFigure() {
+		return (PolylineConnection) ((GraphicalEditPart) getHost()).getFigure();
+	}
+
+	protected void removeSelectionHandles() {
+		getLinkFigure().setForegroundColor(ColorConstants.black);
+	}
+}
