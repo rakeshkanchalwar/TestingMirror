28cbdc9d416e6fd5c6f9e798cc0e413060c6ac72
niting <niting@GEBHSTC9.bitwiseglobal.net>
2015-10-13 17:35:10 +0530

    Adding converters for FixedWidth Components

---
+++ com.bitwise.app.engine/src/main/java/com/bitwise/app/engine/converter/impl/InputFileFixedWidthConverter.java
@@ -0,0 +1,60 @@
+package com.bitwise.app.engine.converter.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+
+import com.bitwise.app.common.util.LogFactory;
+import com.bitwise.app.engine.converter.InputConverter;
+import com.bitwise.app.engine.exceptions.PhaseException;
+import com.bitwise.app.engine.exceptions.SchemaException;
+import com.bitwise.app.graph.model.Component;
+import com.bitwise.app.graph.model.Link;
+import com.bitwiseglobal.graph.commontypes.TypeInputOutSocket;
+import com.bitwiseglobal.graph.inputtypes.FileFixedWidth;
+import com.bitwiseglobal.graph.itfd.TypeInputDelimitedOutSocket;
+
+public class InputFileFixedWidthConverter extends InputConverter {
+
+	Logger logger = LogFactory.INSTANCE.getLogger(InputFileFixedWidthConverter.class);
+
+	public InputFileFixedWidthConverter(Component component) {
+		super();
+		this.baseComponent = new FileFixedWidth();
+		this.component = component;
+		this.properties = component.getProperties();
+	}
+	
+	@Override
+	public void prepareForXML() throws PhaseException, SchemaException{
+		logger.debug("prepareForXML - Genrating XML data for "+component);
+		super.prepareForXML();
+		FileFixedWidth fileFixedWidth = (FileFixedWidth) baseComponent;
+		FileFixedWidth.Path path = new FileFixedWidth.Path();
+		path.setUri((String) properties.get(PATH));
+		FileFixedWidth.Charset charset = new FileFixedWidth.Charset();
+		charset.setValue(getCharset());
+		
+		fileFixedWidth.setPath(path);
+		fileFixedWidth.setStrict(getBoolean(HAS_HEADER));
+		fileFixedWidth.setSafe(getBoolean(IS_SAFE));
+		fileFixedWidth.setCharset(charset);
+		
+	}
+	@Override
+	protected List<TypeInputOutSocket> getInOutSocket() throws SchemaException {
+		logger.debug("getInOutSocket - Genrating TypeInputOutSocket data for "+component);
+		List<TypeInputOutSocket> outSockets = new ArrayList<>();
+		for (Link link : component.getSourceConnections()) {
+			TypeInputDelimitedOutSocket outSocket = new TypeInputDelimitedOutSocket();
+			outSocket.setId((String) link.getTarget().getProperties().get(NAME));
+			outSocket.setType("");
+			outSocket.setSchema(getSchema());
+			outSocket.getOtherAttributes();
+			outSockets.add(outSocket);
+		}
+		return outSockets;
+	}
+
+}
---
+++ com.bitwise.app.engine/src/main/java/com/bitwise/app/engine/converter/impl/OutputFileFixedWidthConverter.java
@@ -0,0 +1,60 @@
+package com.bitwise.app.engine.converter.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+
+import com.bitwise.app.common.util.LogFactory;
+import com.bitwise.app.engine.converter.OutputConverter;
+import com.bitwise.app.engine.exceptions.PhaseException;
+import com.bitwise.app.engine.exceptions.SchemaException;
+import com.bitwise.app.graph.model.Component;
+import com.bitwise.app.graph.model.Link;
+import com.bitwiseglobal.graph.commontypes.TypeOutputInSocket;
+import com.bitwiseglobal.graph.outputtypes.FileFixedWidth;
+import com.bitwiseglobal.graph.otfd.TypeOutputDelimitedInSocket;
+
+public class OutputFileFixedWidthConverter extends OutputConverter {
+
+Logger logger = LogFactory.INSTANCE.getLogger(OutputFileDelimitedConverter.class);
+	
+	public OutputFileFixedWidthConverter(Component component) {
+		super();
+		this.component = component;
+		this.properties = component.getProperties();
+		this.baseComponent = new FileFixedWidth();
+	}
+	
+	@Override
+	public void prepareForXML() throws PhaseException, SchemaException{
+		logger.debug("prepareForXML - Genrating XML data for "+component);
+		super.prepareForXML();
+		FileFixedWidth fileFixedWidth = (FileFixedWidth) baseComponent;
+		FileFixedWidth.Path path = new FileFixedWidth.Path();
+		path.setUri((String) properties.get(PATH));
+		FileFixedWidth.Charset charset = new FileFixedWidth.Charset();
+		charset.setValue(getCharset());
+		
+		fileFixedWidth.setPath(path);
+		fileFixedWidth.setStrict(getBoolean(HAS_HEADER));
+	    fileFixedWidth.setCharset(charset);
+		
+	}
+
+	@Override
+	protected List<TypeOutputInSocket> getOutInSocket() throws SchemaException {
+		logger.debug("getInOutSocket - Genrating TypeOutputInSocket data");
+		List<TypeOutputInSocket> outputinSockets = new ArrayList<>();
+		for (Link link : component.getTargetConnections()) {
+			TypeOutputDelimitedInSocket outInSocket = new TypeOutputDelimitedInSocket();
+			outInSocket.setId("");
+			outInSocket.setType("");
+			outInSocket.setSchema(getSchema());
+			outInSocket.getOtherAttributes();
+			outInSocket.setFromComponentId((String) link.getSource().getProperties().get(NAME));
+			outputinSockets.add(outInSocket);
+		}
+		return outputinSockets;
+	}
+}
