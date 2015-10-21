24664dbb6e6da9f37360007aec66410e9f47e860
kanchank <kanchank@GEBHSTC1.bitwiseglobal.net>
2015-10-21 14:09:53 +0530

    Sonar cube suggestions.

--- com.bitwise.app.graph.figure/src/main/java/com/bitwise/app/graph/figure/ELTColorConstants.java
+++ com.bitwise.app.graph.figure/src/main/java/com/bitwise/app/graph/figure/ELTColorConstants.java
@@ -3,22 +3,25 @@ package com.bitwise.app.graph.figure;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.swt.graphics.Color;
 
-public interface ELTColorConstants {
+public class ELTColorConstants {
+	
+	private ELTColorConstants()
+	{}
 
-	public final static Color lightRed = new Color(null,235, 176, 182);
-	public final static Color darkRed = new Color(null,191, 52, 114);
-	public final static Color darkGrey = new Color(null,115, 119, 120);
-	public final static Color lightGrey = new Color(null,220, 221, 227);
-	public final static Color lightBlue = new Color(null, 22, 169, 199);
-	public final static Color darkBlue =  new Color(null, 17, 128, 151);
-	public final static Color componentSelectBlue =  new Color(null, 83,126,137);
-	public final static Color black = ColorConstants.black;
+	public static final Color lightRed = new Color(null,235, 176, 182);
+	public static final Color darkRed = new Color(null,191, 52, 114);
+	public static final Color darkGrey = new Color(null,115, 119, 120);
+	public static final Color lightGrey = new Color(null,220, 221, 227);
+	public static final Color lightBlue = new Color(null, 22, 169, 199);
+	public static final Color darkBlue =  new Color(null, 17, 128, 151);
+	public static final Color componentSelectBlue =  new Color(null, 83,126,137);
+	public static final Color black = ColorConstants.black;
 
 	
-	public final static Color bgComponent = lightGrey;
-	public final static Color bgComponentSelected = componentSelectBlue;
-	public final static Color componentBorder = darkGrey;
-	public final static Color componentSelectedBorder = darkBlue;
+	public static final Color bgComponent = lightGrey;
+	public static final Color bgComponentSelected = componentSelectBlue;
+	public static final Color componentBorder = darkGrey;
+	public static final Color componentSelectedBorder = darkBlue;
 	
 	
 }
