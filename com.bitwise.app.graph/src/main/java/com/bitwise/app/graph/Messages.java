5a6404bb73b6b2d76fb90d6bbf3f9fd12e06758d
rahulma <rahulma@GEBHSTC15.bitwiseglobal.net>
2015-10-29 17:34:59 +0530

    Resolved message property issue.

--- com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/Messages.java
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/Messages.java
@@ -3,7 +3,7 @@ package com.bitwise.app.graph;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "com.bitwise.app.graph.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "resources.messages"; //$NON-NLS-1$
 	public static String GRADLE_RUN;
 	public static String XMLPATH;
 	public static String PARAM_FILE;
